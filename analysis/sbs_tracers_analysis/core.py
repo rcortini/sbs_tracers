2f295f055ed026142c56225da2d3751e45f0baf0
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-03-13 15:39:29 +0100

    core.hoomdsim now only accepts gsd format

--- analysis/sbs_tracers_analysis/core.py
+++ analysis/sbs_tracers_analysis/core.py
@@ -2,12 +2,5 @@ import os
 import MDAnalysis as mda
 
 class hoomdsim :
-    def __init__ (self,topology_file,dcd=None) :
-        if dcd is not None :
-            if not os.path.exists(dcd) and topology_file.endswith('.gsd'):
-                u = mda.Universe (topology_file)
-            else :
-                u = mda.Universe (topology_file,dcd,format='DCD')
-        else :
-            u = mda.Universe (topology_file)
-        self.u = u
+    def __init__ (self,topology_file) :
+        self.u = mda.Universe (topology_file)
